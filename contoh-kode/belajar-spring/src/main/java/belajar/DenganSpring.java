91072f3020c4d7e3a7b6efce7947714529891716
Endy Muhardin <endy.muhardin@gmail.com>
2015-10-27 13:48:01 +0700

    contoh menyambungkan object dengan Spring

--- contoh-kode/belajar-spring/src/main/java/belajar/DenganSpring.java
+++ contoh-kode/belajar-spring/src/main/java/belajar/DenganSpring.java
@@ -7,12 +7,19 @@ public class DenganSpring {
     public static void main(String[] args) {
         ApplicationContext aplikasiSpring 
                 = new ClassPathXmlApplicationContext("classpath:spring-ioc.xml");
-        
+        /*
         KoneksiDatabase k = (KoneksiDatabase) aplikasiSpring.getBean("kd");
         
         System.out.println("Host : "+k.getHost());
         System.out.println("Nama DB : "+k.getNamaDatabase());
         System.out.println("Username : "+k.getUsername());
         System.out.println("Password : "+k.getPassword());
+        */
+
+        CustomerDao cd = (CustomerDao) aplikasiSpring.getBean("cd");
+
+        Customer c = new Customer();
+        cd.hapus(c);
+
     }
 }
