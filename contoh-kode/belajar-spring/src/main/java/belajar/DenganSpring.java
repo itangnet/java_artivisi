10c620baf730221387f3c12404a19ad30c7536ab
Endy Muhardin <endy.muhardin@gmail.com>
2015-10-27 14:34:12 +0700

    keluarkan konfigurasi ke text file

--- contoh-kode/belajar-spring/src/main/java/belajar/DenganSpring.java
+++ contoh-kode/belajar-spring/src/main/java/belajar/DenganSpring.java
@@ -7,14 +7,12 @@ public class DenganSpring {
     public static void main(String[] args) {
         ApplicationContext aplikasiSpring 
                 = new ClassPathXmlApplicationContext("classpath:spring-ioc.xml");
-        /*
         KoneksiDatabase k = (KoneksiDatabase) aplikasiSpring.getBean("kd");
         
         System.out.println("Host : "+k.getHost());
         System.out.println("Nama DB : "+k.getNamaDatabase());
         System.out.println("Username : "+k.getUsername());
         System.out.println("Password : "+k.getPassword());
-        */
 
         CustomerDao cd = (CustomerDao) aplikasiSpring.getBean("cd");
 
